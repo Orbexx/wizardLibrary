1be86ffe635d1cb28cc52156a4de87aed11ecc5b
David Kesler <dmkesler808@gmail.com>
2025-04-24 18:46:54 -0700

    Wizards Library: The beginning...

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import WizardLogo from './app/assets/img/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <Navbar dark color='primary' sticky='top' expand='md'>
+        <Container>
+          <NavbarBrand href='/'>
+              <img src={WizardLogo} alt='Wizard Logo'/>
+            </NavbarBrand>
+        </Container>
+      </Navbar>
+      Im ready im ready im ready.
+    </div>
+  );
+}
+
+export default App;
